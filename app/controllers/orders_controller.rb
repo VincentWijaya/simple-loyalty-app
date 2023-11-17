2854293f4779ab40f4c80e45abf0b47d9eaabe28
Vincent Wijaya <vincent.wijaya@mekari.com>
2023-11-17 18:07:32 +0700

    remove order relation to customer and refactor create order method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,39 +1,13 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[ show edit update destroy ]
   skip_before_action :verify_authenticity_token
 
-  # GET /orders or /orders.json
-  def index
-    @orders = Order.all
-  end
-
-  # GET /orders/1 or /orders/1.json
-  def show
-  end
-
-  # GET /orders/new
-  def new
-    @order = Order.new
-  end
-
-  # GET /orders/1/edit
-  def edit
-  end
-
-  # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
 
     respond_to do |format|
       if @order.save
         customer = Customer.find_by(customerId: order_params['customerId'])
-        unless customer
-          customer = Customer.new
-          customer.customerId = order_params['customerId']
-          customer.customerName = order_params['customerName']
-          customer.tierId = 1 # Set default tiers for new customer
-          customer.save!
-        end
+        find_or_create_customer unless customer
 
         format.json { render :show, status: :created, location: @order }
       else
@@ -42,37 +16,18 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1 or /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1 or /orders/1.json
-  def destroy
-    @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:customerId, :customerName, :orderId, :totalInCents, :date)
-    end
+  def order_params
+    params.require(:order).permit(:customerId, :customerName, :orderId, :totalInCents, :date)
+  end
+
+  def find_or_create_customer
+    customer = Customer.new
+    puts customer.to_json
+    customer.customerId = order_params['customerId']
+    customer.customerName = order_params['customerName']
+    customer.tierId = 1 # Set default tiers for new customer
+    customer.save!
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,2 @@
 class Order < ApplicationRecord
-  belongs_to :Customer, primary_key: 'customerId'
 end
